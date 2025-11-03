805721e3a3585295628fd3d216a006b9a7ea0de2
LinkunGao <gaolinkun123@gmail.com>
2025-11-03 21:55:59 +1300

    remove router

--- annotator-frontend/src/layouts/segmentation-layout/View.vue
+++ annotator-frontend/src/layouts/segmentation-layout/View.vue
@@ -1,9 +1,14 @@
 <template>
-  <v-main>
+  <!-- <v-main>
     <router-view />
+  </v-main> -->
+  <v-main>
+    <MainPage />
   </v-main>
 </template>
 
 <script lang="ts" setup>
-//
+import MainPage from '@/views/MainPage.vue';
 </script>
+<style scoped>
+</style>
